c38d9fd20310fafa8dc81726d1135871fc61c14f
Ricardo Cardoso <84364125+ricardocardoso90@users.noreply.github.com>
2023-06-07 11:12:20 -0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.header}>
       <img src={igniteLogo} alt="Logotipo do Ignite" />
-      <strong>Event Platform</strong>
+      <strong>Ignite Feed</strong>
     </header>
   )
 };
